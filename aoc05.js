0e493461056501966e614195d0722ab94fbea74f
Abraham Segers <bramsegers@users.noreply.github.com>
2024-12-28 05:41:13 +0100

    Update aoc05.js

--- aoc05.js
+++ aoc05.js
@@ -10,20 +10,20 @@ let v = u => !u.some((a, i) => u.some((b, j) => j > i && f(b, a)))
 let p1 = updts.fold((r, u) => r + v(u) * u[len(u) >> 1], 0)
 
 let topo_sort = (n, a) => {
-	let q = []
-	let v = arr(n, 0)
-	let f = i => (v[i] = 1, a[i].for(j => v[j] || f(j)), q.push(i))
-	v.for((e, i) => e || f(i))
-	return q.reverse()
+  let q = []
+  let v = arr(n, 0)
+  let f = i => (v[i] = 1, a[i].for(j => v[j] || f(j)), q.push(i))
+  v.for((e, i) => e || f(i))
+  return q.reverse()
 }
 
 let p2 = updts.fold((r, u) => {
-	if (v(u)) return r
-	let x = n => u.indexOf(n), c = []
-	let m = u.for(a => u.for(b => f(a, b) && c.push([x(a), x(b)])))
-	let a = c.fold((a, [e, f]) => (a[e].push(f), a), c.map(_ => []))
-	let s = topo_sort(m = len(u), a)
-	return +u[s[m >> 1]] + r
+  if (v(u)) return r
+  let x = n => u.indexOf(n), c = []
+  let m = u.for(a => u.for(b => f(a, b) && c.push([x(a), x(b)])))
+  let a = c.fold((a, [e, f]) => (a[e].push(f), a), c.map(_ => []))
+  let s = topo_sort(m = len(u), a)
+  return +u[s[m >> 1]] + r
 }, 0)
 
 log({ p1 })
