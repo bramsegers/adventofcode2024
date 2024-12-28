023d705de57be3c0b5513ef4ca4d1bb4afdc6669
Abraham Segers <bramsegers@users.noreply.github.com>
2024-12-28 05:58:36 +0100

    Update aoc13.js

--- aoc13.js
+++ aoc13.js
@@ -1,18 +1,18 @@
 require('./_aoc')
 
 let mac = read('aoc13.txt')
-.	join('_')
-.	split('__')
-.	map(e => e
-.	match(/\d+/g)
-.	map(e => +e))
+. join('_')
+. split('__')
+. map(e => e
+. match(/\d+/g)
+. map(e => +e))
 
 let f = n => mac.fold((t, [p, q, r, s, x, y]) => {
-	let u = p * (y + n) - q * (x + n)
-	let v = p * s - q * r
-	let b = u / v
-	let a = (x + n - b * r) / p
-	return a % 1 + b % 1 ? t : t + 3 * a + b
+  let u = p * (y + n) - q * (x + n)
+  let v = p * s - q * r
+  let b = u / v
+  let a = (x + n - b * r) / p
+  return a % 1 + b % 1 ? t : t + 3 * a + b
 }, 0)
 
 log({ p1: f(0) })
